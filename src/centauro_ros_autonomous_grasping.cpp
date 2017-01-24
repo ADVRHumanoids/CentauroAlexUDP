67e6a118c6e62394aa750652bd62f3f253265755
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-01-24 17:23:09 +0100

    Working gataway node (but mind the hack! M(0,0) is the time!)

--- src/centauro_ros_autonomous_grasping.cpp
+++ src/centauro_ros_autonomous_grasping.cpp
@@ -3,36 +3,66 @@
 #include <boost/bind.hpp>
 #include <eigen_conversions/eigen_msg.h>
 #include <CentauroUDP/pipes.h>
+#include <tf2_ros/transform_listener.h>
 
 
-void callback ( geometry_msgs::TransformStamped::ConstPtr msg, int fd){
-
-    Eigen::Affine3d pose;
-
-    tf::transformMsgToEigen(msg->transform, pose);
-
-    int bytes = write(fd, (void *)(&pose), sizeof(Eigen::Affine3d));
-
-
-
-
-}
 
 int main(int argc, char** argv)
 {
+
+    std::cout << "optitrack_to_xddp initialized!" << std::endl;
+
+
     ros::init(argc, argv, "optitrack_to_xddp");
     ros::NodeHandle nh;
 
-    int optitrack_fd = open((pipe_prefix+std::string("optitrack_pipe")).c_str(), O_WRONLY);
+    int optitrack_fd = open((pipe_prefix+std::string("optitrack_pipe2")).c_str(), O_WRONLY | O_CREAT, 0777);
+
+    std::cout << "optitrack_fd: " << optitrack_fd << " - Errno: " << errno << std::endl;
+    perror("open");
+    std::cout << (pipe_prefix+std::string("optitrack_pipe")).c_str() << std::endl;
 
 
     Eigen::Affine3d ee_pose;
     ee_pose.setIdentity();
 
-    ros::Subscriber sub = nh.subscribe<geometry_msgs::TransformStamped>("Operator/TransformedRee",
-                                                                        1,
-                                                                        boost::bind(callback, _1, optitrack_fd)
-                                                                       );
+
+
+    tf2_ros::Buffer tf_buffer;
+    tf2_ros::TransformListener tf_listener(tf_buffer);
+
+    geometry_msgs::TransformStamped transform;
+
+    ros::Rate loop_rate(100);
+
+    while(ros::ok()){
+
+        try{
+
+            std::cout << "Before lookupTransform" << std::endl;
+
+            transform = tf_buffer.lookupTransform("world", "Operator/TransformedRee", ros::Time(0));
+
+            Eigen::Affine3d pose;
+            pose.setIdentity();
+
+
+            tf::transformMsgToEigen(transform.transform, pose);
+            pose.linear()(0,0) = transform.header.stamp.toSec();
+
+            std::cout << "Ref from optitrack : " << pose.translation().transpose() << " " << transform.header.stamp.toSec() << std::endl;
+
+            int bytes = write(optitrack_fd, (void *)(&pose), sizeof(pose));
+        }
+        catch(tf2::TransformException e){
+            std::cout << "dio porchissimo" << e.what() << std::endl;
+        }
+
+        loop_rate.sleep();
+
+    }
+
+
     ros::spin();
 
     return 0;
